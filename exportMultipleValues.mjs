2f65c831abcbf43bcf970b462565e070f129f734
valpaliVenkatesh <valpalivenkatesh1@gmail.com>
2022-04-30 10:43:02 +0000

    solution

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,6 @@
+const myArray = ["camel", 265, true, "5.6"];
+const bulb = { watts: 10, type: "LED" };
+const multiplyByFour = function (number) {
+  return number * 4;
+};
+export { myArray, bulb, multiplyByFour };
